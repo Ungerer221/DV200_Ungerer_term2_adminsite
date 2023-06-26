c4ba6534074f7b207649a86f5efd21f33364c6a2
Ungerer221 <113917241+Ungerer221@users.noreply.github.com>
2023-06-26 13:43:43 +0200

    Delete admin site directory

--- admin site/main.js
+++
@@ -1,9 +0,0 @@
-function login() {
-    // document.getElementById("login-btn").alert
-    alert("working");
-}
-
-function test() {
-    // document.getElementById("login-btn").alert
-    alert("working");
-}
